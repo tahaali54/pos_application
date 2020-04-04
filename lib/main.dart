21a5014a931de49796432b2a641d4db865e3abca
Taha Ali <tahaali54@gmail.com>
2020-04-04 23:05:05 +0500

    fix orientation

--- lib/main.dart
+++ lib/main.dart
@@ -23,13 +23,11 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return LayoutBuilder(builder: (context, constraints) {
-      if (constraints.maxWidth < constraints.maxHeight) {
-        GlobalCache.instance.setSizes(constraints.maxWidth);
-        //GlobalCache.instance.screenHeight = constraints.maxHeight;
-        return PortraitPage();
-      } else {
-        return Container(child: Text('Landscape'));
-      }
+      return OrientationBuilder(builder: (context, orientation) {
+        return orientation == Orientation.portrait
+            ? PortraitPage()
+            : Container(child: Text('Landscape'));
+      });
     });
   }
 }
